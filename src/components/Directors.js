c2f87288bbdd176108787dde65ef0b54fcf65165
mariah622 <mjustine622@gmail.com>
2022-02-11 23:53:32 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,23 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h2>Director: {director.name}</h2>
+          <h2>Movies:</h2>
+          <ul> 
+
+          {director.movies.map((movie, index) => (
+            <li key={index}>
+              <p>{movie}</p>
+            </li>
+          ))}
+
+          </ul>
+
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,47 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie,index) => (
+        <div key={index}>
+          <h2>Movie: {movie.title}</h2>
+          <h4>Time: {movie.time}</h4>
+          <h4>Genres:</h4>
+          <h4>Metascore: {movie.metascore}</h4>
+          <ul>
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+
+          </ul>
+          
+
+      
+
+        </div>
+
+      ))}
     </div>
   );
 };
 
 export default Movies;
+
+// return (
+//   <div>
+//     <h1>Actors Page</h1>
+//     {actors.map((actor,index) => (
+//       <div key={index}>
+//         <h2>Name: {actor.name}</h2>
+//         <h3>Movies:</h3>
+//         <ul>
+//           {actor.movies.map((movie,index) => (
+//             <li key={index}>{movie}</li>
+//           ))}
+
+//         </ul>
+
+//       </div>
+//     ))}
+//   </div>
+// );
